3e32c3057c6d2f251d7f0f87de90f03a8d45c181
youssefnone <113144168+youssefnone@users.noreply.github.com>
2022-09-18 14:20:42 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -21,6 +21,7 @@ repo sync -j80
 
 # add trees
 git clone --depth=1 https://github.com/youssefnone/android_vendor_samsung_m10lte vendor/samsung/m10lte
+git clone --depth=1 https://github.com/youssefnone/android_kernel_samsung_m10lte kernel/samsung/m10lte
+git clone --depth=1 https://github.com/youssefnone/android_device_samsung_m10lte device/samsung/m10lte
 . build/envsetup.sh
 brunch m10lte
-make vendor
