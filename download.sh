378fcc101f333769cb61287c3492b3087622481a
youssefnone <113144168+youssefnone@users.noreply.github.com>
2022-09-18 11:32:22 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip python -y
+# install package
+sudo apt install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip python -y
+# run repo
 
 mkdir ~/bin
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 sudo ln -sf ~/bin/repo /usr/bin/repo
 
+# set configs
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 
+# sync carbon rom
 mkdir -p ~/carbon
 cd ~/carbon
-repo init -u https://github.com/CarbonROM/android.git -b cr-8.0
+repo init --depth=1 -u https://github.com/CarbonROM/android.git -b cr-8.0
 repo sync -j80
+
+# add trees
