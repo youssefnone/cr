be8fb4556c0f5abbb58748f649569db23c8ac829
youssefnone <113144168+youssefnone@users.noreply.github.com>
2022-09-20 08:13:57 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -28,4 +28,27 @@ git clone --depth=1 https://github.com/youssefnone/android_vendor_samsung_univer
 brunch m10lte
 make vendorimage
 
-ls out/target/product/generic
+if [ -z "$TIMEOUT" ];then
+    TIMEOUT=20160
+fi
+
+mv out/target/product/generic/vendor-quemu.img $(pwd)
+
+# Upload to WeTransfer
+# NOTE: the current Docker Image, "registry.gitlab.com/sushrut1101/docker:latest", includes the 'transfer' binary by Default
+transfer wet vendor-qemu.img > link.txt || { echo "ERROR: Failed to Upload the Build!" && exit 1; }
+
+# Mirror to oshi.at
+curl -T vendor-qemu.img https://oshi.at/vendor-quemu.img/${OUTPUT} > mirror.txt || { echo "WARNING: Failed to Mirror the Build!"; }
+
+DL_LINK=$(cat link.txt | grep Download | cut -d\  -f3)
+MIRROR_LINK=$(cat mirror.txt | grep Download | cut -d\  -f1)
+
+# Show the Download Link
+echo "=============================================="
+echo "Download Link: ${DL_LINK}" || { echo "ERROR: Failed to Upload the Build!"; }
+echo "Mirror: ${MIRROR_LINK}" || { echo "WARNING: Failed to Mirror the Build!"; }
+echo "=============================================="
+
+DATE_L=$(date +%d\ %B\ %Y)
+DATE_S=$(date +"%T")
