b020efdbd58aebc4003240419c7bef4f53c42014
youssefnone <113144168+youssefnone@users.noreply.github.com>
2022-09-21 09:28:49 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# install package
+# install package.
 sudo apt install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip python -y
 # run repo.
 
