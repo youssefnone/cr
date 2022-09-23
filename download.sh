1a7d148ff12ea0f32be2a7af294cd616d11cc96f
youssefnone <113144168+youssefnone@users.noreply.github.com>
2022-09-23 13:27:40 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -13,15 +13,20 @@ sudo ln -sf ~/bin/repo /usr/bin/repo
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 
-# sync rom
-cd
-mkdir rom
-cd rom
-repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-17.1
-git clone https://github.com/youssefnone/android_manifest_samsung_m10lte.git .repo/local_manifests
-repo sync --no-repo-verify -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j700
-. build/envsetup.sh && lunch lineage_m10lte-userdebug && mka clean && mka api-stubs-docs && mka hiddenapi-lists-docs && mka system-api-stubs-docs && mka test-api-stubs-docs && mka bacon -j120
+# Create dirs
+mkdir crdroid ; cd crdroid
 
+# Init repo
+repo init --depth=1 -u https://github.com/crdroidandroid/android.git -b 10.0
+
+# Clone my local repo
+git clone https://github.com/youssefnone/android_manifest_samsung_m10lte.git -b crdroid .repo/local_manifests
+
+# Sync
+repo sync --no-repo-verify -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j`nproc`
+
+# Build
+. build/envsetup.sh && lunch lineage_m10lte-eng && mka clean && mka api-stubs-docs && mka hiddenapi-lists-docs && mka system-api-stubs-docs && mka test-api-stubs-docs && mka bacon -j`nproc`
 
 TIMEOUT=20160
 cd ~/rom
