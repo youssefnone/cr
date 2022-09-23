9285b86260b025c8022294e170b9a1367294a3be
youssefnone <113144168+youssefnone@users.noreply.github.com>
2022-09-23 17:13:16 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # install package.
+sudo apt update
 sudo apt install tmate git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip python -y
 # run repo.
 
