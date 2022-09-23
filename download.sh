3d4838a959b2156d8b7cf5604a90eef6a217f88a
youssefnone <113144168+youssefnone@users.noreply.github.com>
2022-09-23 17:19:23 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # install package.
-sudo apt update -y
-sudo apt install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip python -y
+#sudo apt update -y
+#sudo apt install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip python -y
 # run repo.
 
 mkdir ~/bin
@@ -18,7 +18,7 @@ git config --global user.name "Your Name"
 mkdir crdroid ; cd crdroid
 
 # term
-sudo apt install tmate -y
+sudo apt-get update && sudo apt install tmate -y
 tmate 
 
 # Init repo
