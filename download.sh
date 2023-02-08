b64080063b15679417d3a158a9984738500d1854
youssefnone <113144168+youssefnone@users.noreply.github.com>
2023-02-08 16:41:00 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -18,16 +18,16 @@ git config --global user.name "Your Name"
 mkdir crdroid ; cd crdroid
 
 # Init repo
-repo init --depth=1 -u https://github.com/crdroidandroid/android.git -b 10.0
+repo init --depth=1 -u https://github.com/crdroidandroid/android.git -b cr-9.0
 
 # Clone my local repo
-git clone https://github.com/youssefnone/android_manifest_samsung_m10lte.git -b crdroid .repo/local_manifests
+git clone https://github.com/youssefnone/android_manifest_samsung_m10lte.git -b cr-9.0 .repo/local_manifests
 
 # Sync
 repo sync --no-repo-verify -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j`nproc`
 
 # Build
-. build/envsetup.sh && lunch lineage_m10lte-eng && mka clean && mka api-stubs-docs && mka hiddenapi-lists-docs && mka system-api-stubs-docs && mka test-api-stubs-docs && mka bacon -j`nproc`
+. build/envsetup.sh && lunch carbon_m10lte-eng && mka clean && mka bacon -j`nproc`
 
 TIMEOUT=20160
 ls out/target/product/m10lte
